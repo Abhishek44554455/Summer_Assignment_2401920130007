d8ff2041bfe44d2cbcaa323fd3d64e0499cd6507
Abhishek Anand <sauravk8825@gmail.com>
2026-06-02 20:05:22 +0530

    day2 done

---
+++ week_1/day2/maximumAverageSubarrayI.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    double findMaxAverage(vector<int>& nums, int k) {
+        double sum=0;
+        double maxSum;
+        for(int i=0;i<k;i++){
+            sum+=nums[i];
+        }
+        maxSum=sum;
+        for(int i=k;i<nums.size();i++){
+            sum=sum+nums[i]-nums[i-k];
+            maxSum=max(maxSum,sum);
+        }
+        return maxSum/k;
+    }
+};
